9c515f25f1883e8e1d54998150a8077b159f921e
shockwave740 <116198999+shockwave740@users.noreply.github.com>
2025-07-30 16:10:15 -0600

    set hello from me

--- test_flask.py
+++ test_flask.py
@@ -1,10 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from Zach Majeski in 3308!'
-
+    return 'It works!'
 
 if __name__ == '__main__':
     app.run(debug=True)
+
